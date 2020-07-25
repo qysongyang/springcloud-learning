30a26c97178eeeffe7f4f60166634e134db20006
刘松洋 <1083294586@qq.com>
2020-07-25 19:29:50 +0800

    spring learning

---
+++ eureka-consumer/src/test/java/com/lsy/spring/eureka/consumer/EurekaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsy.spring.eureka.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
